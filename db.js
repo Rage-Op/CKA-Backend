18b10f95b903d31637d83815a4af134441820932
Rage-Op <pravinyt1122334455@gmail.com>
2025-10-01 23:56:44 -0500

    Add console log comment in db.js for clarity

--- db.js
+++ db.js
@@ -1,22 +1,22 @@
-const { MongoClient } = require("mongodb");
+import { MongoClient } from "mongodb";
+
 const atlasURI =
   "mongodb+srv://pravinyt1122334455:testing123@development.vc5a8.mongodb.net/?retryWrites=true&w=majority&appName=Development";
 const localDb = "mongodb://localhost:27017/CKA-DB";
 
 let dbConnection;
 
-module.exports = {
-  connectToDb: (cb) => {
-    MongoClient.connect(atlasURI)
-      .then((client) => {
-        console.log("database connected");
-        dbConnection = client.db();
-        return cb();
-      })
-      .catch((err) => {
-        console.log(err);
-        return cb(err);
-      });
-  },
-  getDb: () => dbConnection,
+export const connectToDb = (cb) => {
+  MongoClient.connect(atlasURI)
+    .then((client) => {
+      console.log("database connected");
+      dbConnection = client.db();
+      return cb();
+    })
+    .catch((err) => {
+      console.log(err);
+      return cb(err);
+    });
 };
+
+export const getDb = () => dbConnection;
